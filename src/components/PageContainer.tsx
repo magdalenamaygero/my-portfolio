78d03959c9f3bfa9a6525941b699133b087dcae2
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2025-06-20 15:03:12 -0400

    update

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function PageContainer({ children }: { children: React.ReactNode }) {
   return (
-<div className="bg-white rounded-md shadow-lg border border-gray-100 w-full max-w-[48rem] px-4 sm:px-6 mx-auto space-y-12">
+    <div className="w-full px-4 sm:px-6 md:px-8 max-w-3xl mx-auto py-8 space-y-12 shadow-2xl">
       {children}
     </div>
   );
 }
+
