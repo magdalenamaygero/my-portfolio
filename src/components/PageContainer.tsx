d274d4b156c864c21f84247f7cba79e30ed23e42
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2025-06-20 15:21:50 -0400

    fix container and responsiveness

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function PageContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full px-4 sm:px-6 md:px-8 max-w-3xl mx-auto py-8 space-y-12 shadow-2xl">
+    <div className="w-full px-4 sm:px-6 md:px-8 max-w-3xl mx-auto py-8 shadow-2xl">
       {children}
     </div>
   );
