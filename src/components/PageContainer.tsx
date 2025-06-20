260b6e541811aeba646f909f63a655ce65e50d84
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2025-06-20 14:53:34 -0400

    responsive mobile

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function PageContainer({ children }: { children: React.ReactNode }) {
   return (
-<div className="bg-white rounded-md shadow-lg border border-gray-100 p-8">
+<div className="bg-white rounded-md shadow-lg border border-gray-100 w-full max-w-[48rem] px-4 sm:px-6 mx-auto space-y-12">
       {children}
     </div>
   );
