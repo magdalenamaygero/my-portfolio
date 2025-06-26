0756c30473d043c3e2ce4a8d132d0f454d23de5a
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2025-06-26 19:33:25 -0400

    Fix ExperienceCard rendering

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -7,13 +7,13 @@ type Props = {
 };
 
 export default function ExperienceCard({ title, items, dates }: Props) {
-    return (
-        <div className="border border-purple-300 bg-gray-50 rounded-lg p-6 shadow-lg text-left w-full">
-            <h3 className="text-xl font-semibold text-gray-800 mb-2">{title}</h3>
-            <p>{dates}</p>
-            <p className="text-gray-600 font-serif leading-7 whitespace-pre-line">
-            {items}
-            </p>  
-        </div>
-    )
+  return (
+    <div className="border border-purple-300 bg-gray-50 rounded-lg p-6 shadow-lg text-left w-full">
+      <h3 className="text-xl font-semibold text-gray-800 mb-2">{title}</h3>
+      <p className="text-sm text-gray-500 mb-4">{dates}</p>
+      <p className="text-gray-600 font-serif leading-7 whitespace-pre-line">
+        {items}
+      </p>
+    </div>
+  );
 }
